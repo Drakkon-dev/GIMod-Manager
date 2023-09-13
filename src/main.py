f91a7bacaef4b4793ff08a976801ec5863cb7202
Drakkon-dev <82536159+Drakkon-dev@users.noreply.github.com>
2023-09-13 20:22:06 +0200

    Refactor and optimization
    Pre-mature optimization is horrible but this needs to be as fast as possible, for python code

--- src/main.py
+++ src/main.py
@@ -35,6 +35,11 @@ class App:
         self.settings_path = "settings.json"
         self.version = "0.1"
         self.console_hwnd = 0
+        self.window_size = [0, 0]
+        self.window_position = [0, 0]
+        self.flags = None
+        self.screen = None
+        self.hwnd = 0
         try:
             with open(self.settings_path, "r") as json_data:
                 json_file_data = re.sub(r'//.*', '', json_data.read())
@@ -47,14 +52,6 @@ class App:
             #self.__register_quit__()
         else:
             pass
-        self.window_size = [1, 1]
-        self.window_position = [0, 0]
-        self.flags = pygame.NOFRAME | pygame.SRCALPHA
-        self.screen = pygame.display.set_mode(self.window_size, self.flags)
-        pygame.display.set_caption(self.settings_data["AppTitle"])
-        self.hwnd = pygame.display.get_wm_info()["window"]
-        self.__pygame_window_thing__()
-        self.__hide_window__(True)
 
     def __pygame_window_thing__(self):
         win32gui.SetWindowPos(self.hwnd, Constants.HWND_TOPMOST, 0, 0, 0, 0, Constants.SWP_NOMOVE | Constants.SWP_NOSIZE)
@@ -124,12 +121,17 @@ class App:
         self.__pygame_window_thing__()
     def __set_window_position__(self, x: int, y: int):
         self.window_position = [round(x), round(y)]
-        self.__window_set__()
-    def __window_set__(self):
         win32gui.SetWindowPos(self.hwnd, 0, round(self.window_position[0]), round(self.window_position[1]), 0, 0, Constants.SWP_NOSIZE)
     def __hide_window__(self, hide: bool):
-        if hide:win32gui.ShowWindow(self.hwnd, Constants.SW_HIDE)
-        else:win32gui.ShowWindow(self.hwnd, Constants.SW_SHOWNA)
+        win32gui.ShowWindow(self.hwnd, Constants.SW_HIDE if hide else Constants.SW_SHOWNA)
+        # if hide:
+        #   win32gui.ShowWindow(self.hwnd, Constants.SW_HIDE)
+        # else:
+        #   win32gui.ShowWindow(self.hwnd, Constants.SW_SHOWNA)
+
+    def __refresh_window__(self, width: int, height: int, x: int, y: int):
+        self.__set_window_size__(width, height)
+        self.__set_window_position__(x, y)
 
     def main(self):
         found_game_proc: bool = False
@@ -153,40 +155,46 @@ Looking for PROC: {self.settings_data['AnimeGamePROC']}
                 except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
                     pass
         if found_game_proc:
+            self.window_size = [10, 10]
+            self.window_position = [0, 0]
+            self.flags = pygame.NOFRAME
+            self.screen = pygame.display.set_mode(self.window_size, self.flags)
+            pygame.display.set_caption(self.settings_data["AppTitle"])
+            self.hwnd = pygame.display.get_wm_info()["window"]
+            self.__pygame_window_thing__()
+            self.__hide_window__(True)
+
             self.__main__()
 
     def __main__(self):
         prev_game_window_size: list[int, int] = self.__get_game_window_size__()
         prev_game_window_position: list[int, int] = self.__get_game_window_position__()
-        self.__set_window_size__(
+        self.__refresh_window__(
             prev_game_window_size[0] * (self.settings_data["SizeRelative"][0]/100),
-            prev_game_window_size[1] * (self.settings_data["SizeRelative"][1]/100)
-        )
-        self.__set_window_position__(
+            prev_game_window_size[1] * (self.settings_data["SizeRelative"][1]/100),
             prev_game_window_position[0] + (prev_game_window_size[0]*(self.settings_data["PostionRelative"][0])),
             prev_game_window_position[1] + (prev_game_window_size[1]*(self.settings_data["PostionRelative"][1]))
         )
         if not self.settings_data["Debug"]:
-                win32gui.ShowWindow(self.console_hwnd, Constants.SW_HIDE)
+            win32gui.ShowWindow(self.console_hwnd, Constants.SW_HIDE)
         while True:
             #os.system('cls' if os.name == 'nt' else 'clear')
             self.screen.fill((255, 0, 128))
+            game_window_size = self.__get_game_window_size__()
+            game_window_position = self.__get_game_window_position__()
+            game_window_focused = self.__is_game_focused__()
+
             events = pygame.event.get()
             for event in events:
-                if event.type == pygame.QUIT or not self.__is_game_focused__():
+                if event.type == pygame.QUIT or not game_window_focused:
                     if not self.__is_game_process_running__():
                         self.__register_quit__()
-            
-            if self.__is_game_focused__():
-                game_window_size = self.__get_game_window_size__()
-                game_window_position = self.__get_game_window_position__()
+
+            if game_window_focused:
                 if (game_window_size != prev_game_window_size) or (game_window_position != prev_game_window_position):
-                    print(f"{self.window_size}")
-                    self.__set_window_size__(
+                    self.__refresh_window__(
                         game_window_size[0] * (self.settings_data["SizeRelative"][0]/100),
-                        game_window_size[1] * (self.settings_data["SizeRelative"][1]/100)
-                    )
-                    self.__set_window_position__(
+                        game_window_size[1] * (self.settings_data["SizeRelative"][1]/100),
                         game_window_position[0] + (game_window_size[0]*(self.settings_data["PostionRelative"][0]/100)),
                         game_window_position[1] + (game_window_size[1]*(self.settings_data["PostionRelative"][1]/100))
                     )
@@ -198,10 +206,9 @@ Looking for PROC: {self.settings_data['AnimeGamePROC']}
             pygame.draw.circle(self.screen, (0, 0, 0), (250, 250), 200)
             pygame.draw.circle(self.screen, (255, 0, 128), (250, 250), 20)
             
-            
             pygame.display.flip()
-            prev_game_window_size = self.__get_game_window_size__()
-            prev_game_window_position = self.__get_game_window_position__()
+            prev_game_window_size = game_window_size
+            prev_game_window_position = game_window_position
 
 if __name__ == "__main__":
     print("Loading...")
